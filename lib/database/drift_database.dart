02751952f2df74191da4d67dd6ab53bbd5346438
WoodsCho <durnt35@gmail.com>
2023-04-19 00:02:14 +0900

    데이터베이스 쿼리작성

--- lib/database/drift_database.dart
+++ lib/database/drift_database.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'package:path/path.dart' as p;
 import 'package:calendar_scheduler/model/schedule.dart';
 import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
 
 part 'drift_database.g.dart';
 
@@ -9,4 +13,23 @@ part 'drift_database.g.dart';
   ],
 )
 
-class LocalDatabase extends _$LocalDatabase{}
+class LocalDatabase extends _$LocalDatabase{
+  LocalDatabase() : super(_openConnection());
+  Stream<List<Schedule>> watchSchedule(DateTime date) =>
+      (select(schedules)..where((tbl) => tbl.date.equals(date))).watch();
+
+  Future<int> create(SchedulesCompanion data) =>
+      into(schedules).insert(data);
+  Future<int> removeSchedule(int id) =>
+      (delete(schedules)..where((tbl) => tbl.id.equals(id))).go();
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path,'db.sqlite'));
+    return NativeDatabase(file);
+    });
+}
